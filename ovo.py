9415cadf42abb753fd92c0f0a63471ed6d83ab49
daturaman <mpcarter76@gmail.com>
2017-08-09 19:19:14 +0100

    Cost done

--- ovo.py
+++ ovo.py
@@ -3,20 +3,33 @@ import json
 
 class Tariff:
 
-    tariffs = json.loads(open('prices.json').read())
+    tariffs = json.loads(open('prices.json').read()) 
 
     def __init__(self):
         pass
 
     @classmethod
     def cost(cls, power_usage, gas_usage):
+        cost_per_tariff = []
         for tariff in cls.tariffs:
-            annual_power = 0
+            total = 0
             if 'power' in tariff['rates']:
-                annual_power = power_usage * tariff['rates']['power']
-            print("{0} {1}".format(tariff['tariff'], round(annual_power, 2)))
+                total += power_usage * tariff['rates']['power'] 
+            if 'gas' in tariff['rates']:
+                total += gas_usage * tariff['rates']['gas']
+            #Add standing charge for the year
+            months_in_year = 12
+            total += months_in_year * tariff['standing_charge']
+            #Add VAT   
+            vat = 0.05
+            total += vat * total
+            cost_per_tariff.append((tariff['tariff'], round(total, 2)))
+        return sorted(cost_per_tariff, key=lambda t: t[1])
 
-
+    @classmethod
+    def usage(cls, tariff, fuel_type, target_monthly_spend):
+        #return (((month_spend - standing_charge)*12) - added_vat) / kwh_charge 
+        #kwh_charge = cls.tariffs[tariff]['rates'][fuel_type]
 
 
 
@@ -27,4 +40,4 @@ class Tariff:
 #Tariff.cost()
 #Tariff.cost(100,2)
 t = Tariff()
-t.cost(100,2)
+print(t.cost(100,50))
