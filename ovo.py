3d9a56cf259d3a13339f2c2365721750445aa647
Michael Carter <mcarter@gresham-computing.com>
2017-08-11 09:40:29 +0100

    Just testing left

--- ovo.py
+++ ovo.py
@@ -4,7 +4,6 @@ import json
 class Tariff:
 
     tariffs = json.loads(open('prices.json').read())
-    months_in_year = 12
     vat = 0.05
 
     def __init__(self):
@@ -20,16 +19,17 @@ class Tariff:
             if 'gas' in tariff['rates']:
                 total += gas_usage * tariff['rates']['gas']
             #Add standing charge for the year
-            total += cls.months_in_year * tariff['standing_charge']
+            months_in_year = 12
+            total += months_in_year * tariff['standing_charge']
             #Add VAT
             total += cls.vat * total
             cost_per_tariff.append((tariff['tariff'], round(total, 2)))
-        return sorted(cost_per_tariff, key=lambda t: t[1])
+        return set(sorted(cost_per_tariff, key=lambda t: t[1]))
 
     @classmethod
     def usage(cls, tariff, fuel_type, target_monthly_spend):
-        try:            
-            selected_tariff = [t for t in cls.tariffs if t['tariff'] == tariff][0]            
+        try:
+            selected_tariff = [t for t in cls.tariffs if t['tariff'] == tariff][0]
         except IndexError:
             print("{0} is not a recognised tariff.".format(tariff))
             return 0
@@ -45,20 +45,23 @@ class Tariff:
         return round(net_monthly_spend / gross_kwh, 2)
 
 
-#print(Tariff.usage('better-energy', 'power', 100))
-cmd = input("Please enter command:\n").split()
-while cmd[0] != 'quit':
-    if cmd[0] == 'cost':
-        result = Tariff.cost(float(cmd[1]), float(cmd[2]))
-        print(result)
-    elif cmd[0] == 'usage':
-        result = Tariff.usage(cmd[1], cmd[2], float(cmd[3]))
-        print(result)#TODO format me
-    else:
-        cmd = input("Command not recognised. Please try again: \n").split()
-        continue
-    cmd = input("Please enter command:\n").split()
-
-
-
-
+if __name__ == "__main__":
+    import sys
+    cmd = sys.argv[1:]
+    while cmd[0] != 'quit':
+        try:
+            print()
+            if cmd[0] == 'cost':
+                result = Tariff.cost(float(cmd[1]), float(cmd[2]))
+                print(result)
+                [print("{0} {1}".format(t[0], t[1])) for t in result]
+            elif cmd[0] == 'usage':
+                result = Tariff.usage(cmd[1], cmd[2], float(cmd[3]))
+                print(result)
+            else:
+                cmd = input("\nCommand not recognised. Please try again: \n").split()
+                continue
+        except IndexError:
+            cmd = input("\nYou entered a command in an incorrect format. Please try one of \n'cost <POWER_USAGE> <GAS_USAGE>' or \n'usage <TARIFF_NAME> <FUEL_TYPE> <TARGET_MONTHLY_SPEND>': \n").split()
+            continue
+        cmd = input("\nPlease enter command:\n").split()
--- ovotest.py
+++ ovotest.py
@@ -1,13 +1,36 @@
 import unittest
 from ovo import Tariff
 
+#This is the same as a 'Parameterized' test in Junit.
 class TestOvo(unittest.TestCase):
 
+    usage_params = [
+        [('better-energy', 'power', 100), 635.76],
+        [('2yr-fixed', 'gas', 250), 7748.15],
+        [('greener-energy', 'power', 80), 439.51],
+        [('simpler-energy', 'power', 120), 755.99]
+    ]
+
+    cost_params = [
+        [(100, 50), {('better-energy', 120.82), ('greener-energy', 121.17), ('2yr-fixed', 126.47), ('simpler-energy', 126.63)}],
+        [(400, 120), {('simpler-energy', 173.01), ('better-energy', 166.0), ('greener-energy', 169.81), ('2yr-fixed', 172.65)}],
+        [(950, 310), {('greener-energy', 258.97), ('2yr-fixed', 259.24), ('simpler-energy', 260.18), ('better-energy', 250.69)}]
+    ]
+
     def test_usage(self):
-        t = Tariff()
-        result = t.usage('better-energy', 'power', 100)
-        self.assertEqual(result, 635.76)
-    
+        for i in self.usage_params:
+            with self.subTest():
+                args = i[0]
+                expected = i[1]
+                self.assertEqual(Tariff.usage(*args), expected)
+
+    def test_cost(self):
+        for i in self.cost_params:
+            with self.subTest():
+                args = i[0]
+                expected = i[1]
+                self.assertTrue(Tariff.cost(*args) >= expected)
+
 
 if __name__ == '__main__':
     unittest.main()
